2b3980e86f9d4f834e995d1c7e6451731f11f6ef
hema <hema.bana@gmail.com>
2019-03-13 14:47:08 +0530

    new commit

---
+++ src/test/java/Project1/NewProject/API.java
@@ -0,0 +1,16 @@
+package Project1.NewProject;
+
+import org.testng.annotations.Test;
+
+public class API {
+
+	@Test
+	public void sub()
+	
+	{
+		System.out.println("Hi");
+
+	}
+
+
+}
---
+++ src/test/java/Project1/NewProject/SQL.java
@@ -0,0 +1,15 @@
+package Project1.NewProject;
+
+import org.testng.annotations.Test;
+
+public class SQL {
+
+	@Test
+	public void add()
+	
+	{
+		System.out.println("Bye");
+
+	}
+
+}
